172234ee2ddd622937672e776e5d5ed23ea85eec
Anant Anand Gputa <anantanandgupta@gmail.com>
2017-03-05 04:44:45 +0530

    /conver_to_class: done
    plugin code is converted to class

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: MGT Embed Video Link
+ * Plugin URI: https://www.mindzgrouptech.net/wordpress-plugin-embed-video-link
+ * Description: A plugin that renders / embeds YouTube link in posts with just a shortcode.
+ * Version: 0.0.1
+ * Author: MindzGroupTech
+ * Author URI: https://www.mindzgrouptech.net
+ */
+
+define('EVL_PLUGIN_FILE', __FILE__);
+define('EVL_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+require EVL_PLUGIN_PATH.'videolink.php';
+
+new VideoLink();
